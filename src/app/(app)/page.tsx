fa650bfdcfb3929cd6c154c2c8f17884632c4486
iboughtbed <testpythontosanzhar@gmail.com>
2024-12-08 19:52:16 +0500

    chore: disable chat

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,15 +1,26 @@
-import { Chat } from "~/components/chat";
-import { AI } from "~/lib/chat/actions";
-import { generateId } from "~/lib/utils";
+// import { Chat } from "~/components/chat";
+// import { AI } from "~/lib/chat/actions";
+// import { generateId } from "~/lib/utils";
 // import { getCachedUser } from "~/server/auth";
 
-export default async function IndexPage() {
-  const id = generateId();
+export default async function IndexPage({
+  // searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>;
+}) {
+  // const id = generateId();
   // const user = await getCachedUser();
 
+  // const planetName = searchParams.planetName as string;
+
   return (
-    <AI initialAIState={{ chatId: id, messages: [] }}>
-      <Chat id={id} />
-    </AI>
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <h1 className="text-5xl font-semibold">
+        Chat has been stopped temperoraly
+      </h1>
+    </div>
+    // <AI initialAIState={{ chatId: id, messages: [] }}>
+    //   <Chat id={id} />
+    // </AI>
   );
 }
